ce2c06511f363c8726c8a1bc21c1e5bc9d070498
rikiniki <ristevskin@gmail.com>
2026-08-15 18:00:23 +0200

    Refactor data extraction and simplify response

--- main.py
+++ main.py
@@ -4,28 +4,18 @@ from baza import app, zabelezi_naplatu, uzmi_statistiku_zarade, get_connection
 @app.route('/api/naplati', methods=['POST'])
 def api_naplati():
     data = request.json
-    datum = data.get('datum')
-    vreme = data.get('vreme')
-    ime = data.get('ime', '')
-    usluga = data.get('usluga', '')
-    cena = data.get('cena', 0)
-    
-    # 1. Beležimo zaradu
+    datum, vreme, ime, usluga, cena = data.get('datum'), data.get('vreme'), data.get('ime', ''), data.get('usluga', ''), data.get('cena', 0)
     zabelezi_naplatu(datum, vreme, ime, usluga, cena)
-    
-    # 2. Brišemo termin iz baze
     conn = get_connection()
     c = conn.cursor()
     c.execute("DELETE FROM rezervacije WHERE datum = ? AND vreme = ?", (datum, vreme))
     conn.commit()
     conn.close()
-    
     return jsonify({"status": "ok"})
 
 @app.route('/api/statistika', methods=['GET'])
 def api_statistika():
-    statistika = uzmi_statistiku_zarade()
-    return jsonify(statistika)
+    return jsonify(uzmi_statistiku_zarade())
 
 if __name__ == "__main__":
     app.run()
