ec61b71fa5138d00173346d92b086126178dab45
rikiniki <ristevskin@gmail.com>
2026-08-15 17:33:20 +0200

    Add API routes for payment and statistics

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-from baza import app
+from flask import request, jsonify
+from baza import app, zabelezi_naplatu, otkazi_termin, uzmi_statistiku_zarade
+
+@app.route('/api/naplati', methods=['POST'])
+def api_naplati():
+    data = request.json
+    datum = data.get('datum')
+    vreme = data.get('vreme')
+    ime = data.get('ime', '')
+    usluga = data.get('usluga', '')
+    cena = data.get('cena', 0)
+    
+    # 1. Upisujemo u naplate
+    zabelezi_naplatu(datum, vreme, ime, usluga, cena)
+    
+    # 2. Oslobađamo termin iz rasporeda
+    otkazi_termin(datum, vreme)
+    
+    return jsonify({"status": "ok"})
+
+@app.route('/api/statistika', methods=['GET'])
+def api_statistika():
+    statistika = uzmi_statistiku_zarade()
+    return jsonify(statistika)
 
 if __name__ == "__main__":
     app.run()
